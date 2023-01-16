bfe8dc62d246bf965b9bbaca615d42cbee0f729a
Kaius Conway-Lai <kc319@ee-beholder0.ee.ic.ac.uk>
2023-01-16 19:06:58 +0000

    made python module for generic code

---
+++ python/template_code/CodeTemplate.py
@@ -0,0 +1,52 @@
+import json
+from copy import deepcopy
+
+class CodeTemplate:
+    file_path = ""
+    variables = {}
+    template_code = ""
+    output_code = "default"
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_file(self):
+        with open(self.file_path + ".json", 'r') as source:
+            vars = json.load(source)
+            self.variables = vars["variables"]
+
+        with open(self.file_path + ".txt", 'r') as source:
+            self.template_code = source.read()
+
+    def set_variables(self, **kwargs):
+        for arg in kwargs:
+            if self.variables.has_key(arg):
+                self.variables[arg] = kwargs[arg]
+            else:
+                print("{} is not a variable in the template code".format(arg))
+
+    def insert_variables(self):
+        self.output_code = deepcopy(self.template_code)
+        for var in self.variables:
+            placeholder = "${" + var + "}"
+            self.output_code = self.output_code.replace(placeholder, str(self.variables[var]))
+
+
+    def __str__(self):
+        print(self.file_path)
+        print(self.variables)
+        print(self.template_code)
+        print(self.output_code)
+        return "\n"
+
+        
+if __name__ == "__main__":
+    myTemplate = CodeTemplate("templates/verilog_mult")
+    myTemplate.read_file()
+    myTemplate.set_variables(IN1_WIDTH = 32, IN2_WIDTH = 32, OUT_WIDTH = 64)
+    myTemplate.insert_variables()
+    print(myTemplate)
+    with open("out.v", 'w') as os:
+        os.write(myTemplate.output_code)
+
+    

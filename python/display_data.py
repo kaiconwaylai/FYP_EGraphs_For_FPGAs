8854a5dc8057b5dd91f31e33febe0e9bf244839b
kc319 <kaiconwaylai@gmail.com>
2023-01-30 01:17:13 +0000

    added figures and code to display

---
+++ python/display_data.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+def main():
+
+    normal = pd.read_csv('data/1-128normal.csv')
+    karatsuba = pd.read_csv('data/1-128karatsuba.csv')
+    LUT_Graph = normal.plot(x = 'IN1', y = 'LUTs', label = 'Default', title = 'LUT usage against Input Width')
+    karatsuba.plot(ax = LUT_Graph, x = 'IN1', y = 'LUTs', ylabel = 'LUTs', label = 'Karatsuba')
+    DSP_Graph = normal.plot(x = 'IN1', y = 'DSPs', label = 'Default', title = 'DSP usage against Input Width')
+    karatsuba.plot(ax = DSP_Graph, x = 'IN1', y = 'DSPs', ylabel = 'DSPs', label = 'Karatsuba')
+
+    plt.show()
+
+    return 0
+
+if __name__ == "__main__":
+    main()

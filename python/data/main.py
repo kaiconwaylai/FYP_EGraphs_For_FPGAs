7aee7de4c272a74a39586db9548c37352c6cfb31
kc319 <kaiconwaylai@gmail.com>
2023-05-08 13:19:03 +0100

    small python script to convert data to rust hashmap initialiser

---
+++ python/data/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def csv_to_hashmap(input_file):
+    data = pd.read_csv(input_file)
+    with open('out.txt', 'w') as dst:
+        for i,row in data.iterrows():
+            dst.write("({}, fpga::Cost{{dsp: {}, lut: {}}}),\n".format(row[1],row[2],row[3]))
+
+
+
+csv_to_hashmap('1-128addition.csv')

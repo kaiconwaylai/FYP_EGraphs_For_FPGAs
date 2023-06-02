c008f9824eb2aa713bab0a735adfa042c15ba925
Kaius Conway-Lai <kc319@ee-beholder0.ee.ic.ac.uk>
2023-06-02 18:57:37 +0100

    more changes to script to try:)

--- verilog_testbench/run.sh
+++ verilog_testbench/run.sh
@@ -7,7 +7,7 @@
 
 #set VIVADO_BIN_DIR="$RDI_ROOT/prep/rdi/vivado/bin"
 
-VIVADO_BIN_DIR="/mnt/applications/Xilinx/23.1/Vivado/2023.1/bin"
+VIVADO_BIN_DIR="/mnt/applications/Xilinx/20.2/Vivado/2020.2/bin"
 
 OUT_SIM_SNAPSHOT="mult"
 XSI_INCLUDE_DIR="$VIVADO_BIN_DIR/../data/xsim/include"
@@ -19,17 +19,19 @@ OUT_EXE="run_simulation"
 #rm -rf xsim.dir xsim.log xelab* $OUT_EXE
 
 # Compile the HDL design into a simulatable Shared Library
-/mnt/applications/Xilinx/23.1/Vivado/2023.1/bin/xelab work.mult_verilog -prj mult.prj -dll -s $OUT_SIM_SNAPSHOT -debug wave
+/mnt/applications/Xilinx/20.2/Vivado/2020.2/bin/xelab work.mult -prj mult.prj -dll -s $OUT_SIM_SNAPSHOT -debug wave
+
+/mnt/applications/Xilinx/20.2/Vivado/2020.2/data/xsim/include
 
 # Compile the C++ code that interfaces with XSI of ISim
 #$GCC_COMPILER -I$XSI_INCLUDE_DIR  -g -c -o xsi_loader.o xsi_loader.cpp
-$GCC_COMPILER -I$XSI_INCLUDE_DIR  -O3 -c -o xsi_loader.o ./src/xsi_loader.cpp
-$GCC_COMPILER -I$XSI_INCLUDE_DIR  -O3 -c -o register.o ./src/Register.cpp
+$GCC_COMPILER -I /mnt/applications/Xilinx/20.2/Vivado/2020.2/data/xsim/include -O3 -c -o xsi_loader.o ./src/xsi_loader.cpp
+$GCC_COMPILER -I /mnt/applications/Xilinx/20.2/Vivado/2020.2/data/xsim/include -O3 -c -o register.o ./src/Register.cpp
 # Compile the program that needs to simulate the HDL design
 #$GCC_COMPILER -I$XSI_INCLUDE_DIR  -g -c -o testbench.o testbench.cpp
-$GCC_COMPILER -I$XSI_INCLUDE_DIR  -O3 -c -o testbench.o multiplierTestbench.cpp
+$GCC_COMPILER -I /mnt/applications/Xilinx/20.2/Vivado/2020.2/data/xsim/include -O3 -c -o testbench.o multiplierTestbench.cpp
 
-$GCC_COMPILER -ldl -lrt  -o $OUT_EXE testbench.o xsi_loader.o register.o
+$GCC_COMPILER -ldl -lrt -o $OUT_EXE testbench.o xsi_loader.o register.o
 
 # Run the program
 ./$OUT_EXE $1

0b4cd6728891ffb078523af14e69b60c2497ddc4
vagrant <vagrant@vagrant.vm>
2023-06-03 20:22:39 +0000

    add more to set env for vagrant

--- verilog_testbench/set_env.sh
+++ verilog_testbench/set_env.sh
@@ -1,7 +1,10 @@
+
 ##############################################################
 # Copyright (c) 1986-2023 Xilinx, Inc.  All rights reserved. #
 ##############################################################
 
 source '/mnt/applications/Xilinx/20.1/Vivado/2020.1/settings64.sh'
 
+export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LIBRARY_PATH
 export LD_LIBRARY_PATH=/mnt/applications/Xilinx/20.1/Vivado/2020.1/lib/lnx64.o:${LD_LIBRARY_PATH}
+export LM_LICENSE_FILE=$LM_LICENSE_FILE:7182@ee-llic01.ee.ic.ac.uk:7193@ee-llic01.ee.ic.ac.uk:1717@ee-llic01.ee.ic.ac.uk:2100@ee-llic01.ee.ic.ac.uk:27004@ee-llic01.ee.ic.ac.uk:27000@chicken.doc.ic.ac.uk

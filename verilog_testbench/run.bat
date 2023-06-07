d20bee55f8c52f55f16fdff90ec5787314fd0dbd
kc319 <kaiconwaylai@gmail.com>
2023-06-07 17:43:52 +0100

    improved bat file

--- verilog_testbench/run.bat
+++ verilog_testbench/run.bat
@@ -16,9 +16,14 @@
 @REM call del *.pb
 @REM call del *.log
 
+call del /F /Q xsim.dir
+call del *.o
+call del run_xsim.exe
+
 call set install_root="C:\Xilinx\Vivado\2020.2"
 
 call set PATH=C:\Xilinx\Vivado\2020.2/lib/win64.o;C:\Xilinx\Vivado\2020.2/tps/mingw/6.2.0/win64.o/nt/x86_64-w64-mingw32/lib
+
 
 call C:\Xilinx\Vivado\2020.2\bin\xelab work.mult -prj mult.prj -dll -s mult
 
@@ -30,7 +35,9 @@ call "C:\Xilinx\Vivado\2020.2\tps\mingw\6.2.0\win64.o\nt\bin\g++.exe" -I C:\Xili
 
 call "C:\Xilinx\Vivado\2020.2\tps\mingw\6.2.0\win64.o\nt\bin\g++.exe" -o run_xsim testbench.o xsi_loader.o register.o
 
-call run_xsim
+call run_xsim %1
 
 call del *.o
 call del *.pb
+call del *.log
+call del run_xsim.exe

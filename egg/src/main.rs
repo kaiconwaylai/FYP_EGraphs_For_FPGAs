ebecbea92e9218c2820050c67d2c1bcc3ea969e2
Kai <kaiconwaylai@gmail.com>
2023-06-13 15:39:51 +0100

    cchchchchchcangessss

--- egg/src/main.rs
+++ egg/src/main.rs
@@ -27,19 +27,23 @@ fn main() -> std::io::Result<()> {
     
     let mut results = fs::File::create("./output/results.txt")?;
 
+    let runner_iteration_limit = 100;
+    let egraph_node_limit = 25000;
+    let iterations = 10000;
+    let step = 100000.0;
+    let cbc_timeout = 300.0;
+
     let expr: RecExpr<BitLanguage> = input.parse().unwrap();
     let runner = Runner::default()
-        .with_iter_limit(100)
-        .with_node_limit(25000)
+        .with_iter_limit(runner_iteration_limit)
+        .with_node_limit(egraph_node_limit)
         .with_expr(&expr)
         .run(&make_rules());
     let root: Id = runner.roots[0];
 
     let mut unique_solutions = HashSet::new();
 
-    let iterations = 1000;
-    let step = 10000.0;
-    let cbc_timeout = 300.0;
+
 
     for i in 0..iterations+1 {
         alpha(Some(i as f64/step));
